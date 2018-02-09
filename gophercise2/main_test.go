67f962a70fafb0e640d6805e01bd5d04637778e7
tamarabartlett <tbartlett@pivotal.io>
2018-02-09 11:28:52 -0700

    Empty dir and main.go and tests for gophercise2

---
+++ gophercise2/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("The main gophercise2 function", func() {
+	It("should probably do something eventually", func() {
+		Expect("Yay!").To(Equal("Yay!"))
+	})
+})
